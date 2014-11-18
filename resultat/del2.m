5475e26d07df3cf3ee155c6bb7523b88abf8d8c1
Petrus <petrus.jonsson@gmail.com>
2014-11-18 16:42:43 +0100

    la till en mapp med resultat och gjort Del 2, lösa och plotta

---
+++ resultat/del2.m
@@ -0,0 +1,14 @@
+row1 =[1.0 0 0 0 0 0];
+row2 = [0.3 -1.3 0.5 0.5 0 0];
+row3 = [0 0.5 -1.6 0.6 0 0.5];
+row4 = [0 0.5 0.6 -1.6 0.5 0];
+row5 = [0 0 0 0 1.0 0];
+row6 = [0 0 0 0 0 1.0];
+
+A = [row1;row2;row3;row4;row5;row6];
+
+x = A\b;
+bar(x, 'G')
+title('Lösning')
+ylabel('tryck')
+xlabel('Nummer')
